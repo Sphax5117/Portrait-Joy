74dabb3386206e7b0fa933b44bad09d3f0607a10
Sphax5117 <79364242+Sphax5117@users.noreply.github.com>
2023-03-22 15:47:32 +0100

    Update models.py

--- main/models.py
+++ main/models.py
@@ -5,6 +5,6 @@ class Creations(models.Model):
     def __str__(self):
         return f'{self.titre}'
     cree_a = models.DateTimeField(auto_now_add=True)
-    photo = models.ImageField(upload_to='/static/IMAGES',width_field=None, height_field=None)
+    photo = models.ImageField(upload_to='images',width_field=None, height_field=None)
     titre = models.fields.TextField(max_length=50)
     description = models.fields.TextField(max_length=1000)
