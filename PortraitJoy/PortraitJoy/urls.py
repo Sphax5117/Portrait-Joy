3f18f49e221f8c3b05de8bd818efbbb7ff780aaf
Sphax5117 <79364242+Sphax5117@users.noreply.github.com>
2023-03-22 11:31:45 +0100

    test

--- PortraitJoy/PortraitJoy/urls.py
+++ PortraitJoy/PortraitJoy/urls.py
@@ -20,7 +20,7 @@ from django.urls import path
 from main import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
@@ -29,13 +29,7 @@ urlpatterns = [
     path('infos/', views.infos),
     path('tarifs/', views.tarifs),
     path('avis/', views.avis),
-    path('legal/', views.legal)
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('legal/', views.legal),
+    path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
-
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT)
