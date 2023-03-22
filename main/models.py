c5e1940595fa2791e93298e79df6eeb13de78f2d
Sphax5117 <79364242+Sphax5117@users.noreply.github.com>
2023-03-22 16:25:37 +0100

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,16 +1,10 @@
 from distutils.command.upload import upload
 from django.db import models
-import os
-from pathlib import Path
-
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 class Creations(models.Model):
     def __str__(self):
         return f'{self.titre}'
     cree_a = models.DateTimeField(auto_now_add=True)
-    photo = models.ImageField(upload_to=os.path.join(BASE_DIR, 'images/'),width_field=None, height_field=None)
+    photo = models.ImageField(upload_to='static/IMAGES/',width_field=None, height_field=None)
     titre = models.fields.TextField(max_length=50)
     description = models.fields.TextField(max_length=1000)
